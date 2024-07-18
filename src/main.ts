66019aa63ec2d84b4ffa434d8766b30733932b35
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-07-18 20:36:37 +0300

    di container was added

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,7 @@
-import express, { Express } from 'express';
-import { Server } from 'http';
-import { inject, injectable } from 'inversify';
-import { ExeptionFilter } from './errors/exeption.filter';
-import { ILogger } from './logger/logger.interface';
-import { TYPES } from './types';
-import { json } from 'body-parser';
+import express, {Express} from 'express';
+import {Server} from 'http';
+import {injectable} from 'inversify';
 import 'reflect-metadata';
-import { IConfigService } from './config/config.service.interface';
-import { IExeptionFilter } from './errors/exeption.filter.interface';
-import { UserController } from './users/users.controller';
-import { PrismaService } from './database/prisma.service';
-import { AuthMiddleware } from './common/auth.middleware';
 
 @injectable()
 export class App {
@@ -23,22 +14,8 @@ export class App {
         this.app = express();
         this.port = 8000;
     }
-
-    useMiddleware(): void {
-        this.app.use(json());
-        this.app.use(authMiddleware.execute.bind(authMiddleware));
-    }
-
-    useExeptionFilters(): void {
-        this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
-    }
-
     public async init(): Promise<void> {
-        this.useMiddleware();
-        this.useExeptionFilters();
-        await this.prismaService.connect();
         this.server = this.app.listen(this.port);
-        this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
     }
 
     public close(): void {
