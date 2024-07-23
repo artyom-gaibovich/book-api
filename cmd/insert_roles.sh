156fc0ed05ccfb91f8bcc6241ed419bc00cd6c1a
artyom <artyom@andrey7.ad.guu.ru>
2024-07-23 18:43:00 +0300

    remove cmd directory

--- cmd/insert_roles.sh
+++
@@ -1,1 +0,0 @@
-INSERT INTO roles (value) VALUES ('ADMIN'), ('USER');
--- cmd/users.sh
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    UNIQUE (email),
-    UNIQUE (username)
-);
